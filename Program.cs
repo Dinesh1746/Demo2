34f2f99ad951f8280b657cb574a4ecc7074ac2b6
Dinesh <dineshsai1746@gmail.com>
2021-12-01 19:02:51 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Demo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("My nane is Dinesh!");
+        }
+    }
+}
