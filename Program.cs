5f22fb6952762229d9462c24017883abac36ab73
Dinesh <dineshsai1746@gmail.com>
2021-12-02 11:23:31 +0530

    reddy check once

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace Demo2
             Console.WriteLine("Hello World!");
             Console.WriteLine("My nane is Dinesh!");
             Console.WriteLine(" My name is Reddy");
+            Console.WriteLine(" My name is Reddy");
         }
     }
 }
